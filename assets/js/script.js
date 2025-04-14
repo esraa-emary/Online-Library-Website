0891a771da4015eb0e41e5ac10277fcff730d42c
esraa <esraaemary33@gmail.com>
2025-04-14 11:03:03 +0200

    Add About-Us.html and add slider-book in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,74 @@
+// ---------------------------------------- slider-books
+
+const slides1 = document.querySelectorAll(".cards .card");
+const prevBtn = document.querySelector(".prev1");
+const nextBtn = document.querySelector(".next1");
+let slideIndex1 = 1; 
+
+document.addEventListener("DOMContentLoaded", initializeSlider1);
+
+function initializeSlider1() {
+    if (slides1.length > 0) {
+        slides1[1].classList.add("center-slide");
+        showSlides1(slideIndex1);
+        updateButtonStates();
+    }
+}
+
+function showSlides1(index) {
+    slides1.forEach(slide => {
+        slide.style.display = "none";
+        slide.classList.remove("center-slide", "side-slide");
+    });
+
+    const middleIndex = index;
+    const leftIndex = middleIndex - 1 >= 0 ? middleIndex - 1 : slides1.length - 1;
+    const rightIndex = middleIndex + 1 < slides1.length ? middleIndex + 1 : 0;
+
+    slides1[leftIndex].style.display = "block";
+    slides1[leftIndex].classList.add("side-slide");
+    
+    slides1[middleIndex].style.display = "block";
+    slides1[middleIndex].classList.add("center-slide");
+    
+    slides1[rightIndex].style.display = "block";
+    slides1[rightIndex].classList.add("side-slide");
+
+    updateButtonStates();
+}
+
+function nextSlide1() {
+    if (slideIndex1 < slides1.length - 1) {
+        slideIndex1++;
+    } else {
+        slideIndex1 = 0;
+    }
+    showSlides1(slideIndex1);
+}
+
+function prevSlide1() {
+    if (slideIndex1 > 0) {
+        slideIndex1--;
+    } else {
+        slideIndex1 = slides1.length - 1;
+    }
+    showSlides1(slideIndex1);
+}
+
+function updateButtonStates() {
+    if (slideIndex1 == 1) {
+        prevBtn.classList.add("disabled");
+    } else {
+        prevBtn.classList.remove("disabled");
+    }
+    
+    if (slideIndex1 >= slides1.length - 3) {
+        nextBtn.classList.add("disabled");
+    } else {
+        nextBtn.classList.remove("disabled");
+    }
+}
+
 // ---------------------------------------- feed-back
 
 const slides = document.querySelectorAll(".feed-slides div");
