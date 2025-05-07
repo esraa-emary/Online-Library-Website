26e8b0d7c2019e89ee7934393ebc973f516bba1c
George Malak <georgmalak2004@gmail.com>
2025-05-08 02:00:19 +0300

    Edit with Nabil git add . add DataBase git add .

--- project/Pages/admin.py
+++ project/Pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User,Book,Category
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Category)
+
 
 # Register your models here.
