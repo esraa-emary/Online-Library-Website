bdcdf96b461d5060e4d644a235d3822b27bd00ec
mohamednabil75 <mohamednabil20057@gmail.com>
2025-05-10 12:20:36 +0300

    .

--- project/Pages/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User,Book,Category
-admin.site.register(User)
-admin.site.register(Book)
-admin.site.register(Category)
-
-
-# Register your models here.
